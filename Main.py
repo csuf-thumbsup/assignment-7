57d4b44189678ac82c708ec01933bd62d0f4aa8e
Rogelio Negrete <rogelio_negrete@live.com>
2016-11-01 10:31:59 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -43,10 +43,10 @@ def run_parser(parsing_table, input_str, terminals, starting_non_terminal):
 
 
 if __name__ == '__main__':
-    '''
+    
     # Data for Problem #1
 
-    parsing_table = {
+    parsing_table_1 = {
         'E': {'i': 'TQ',
               '+': 'undef',
               '-': 'undef',
@@ -96,70 +96,13 @@ if __name__ == '__main__':
 
 
     # don't include $ since it's the Ending Terminal
-    terminals = ['i', '+', '-', '*', '/', '(', ')']
-    input_str = '(i+i)*i$'
-    starting_non_terminal = 'E'
+    terminals_1 = ['i', '+', '-', '*', '/', '(', ')']
+    input_strs_1 = ['(i+i)*i$', 'i*(i-i)$', 'i(i+i)$']
+    starting_non_terminal_1 = 'E'
 
-    '''
+    
     # Data for Problem #2
-    _old_parsing_table = {
-        'S': {'a': 'F=E',
-              'b': 'F=E',
-              '(': 'F=E',
-              ')': 'undef',
-              '+': 'undef',
-              '*': 'undef',
-              '$': 'undef',
-              '=': 'undef'
-              },
-        'E': {'a': 'TQ',
-              'b': 'TQ',
-              '(': 'TQ',
-              ')': 'undef',
-              '+': 'undef',
-              '*': 'TQ',
-              '$': 'undef',
-              '=': 'undef'
-              },
-        'Q': {'a': 'undef',
-              'b': 'undef',
-              '(': 'undef',
-              ')': 'λ',
-              '+': '+TQ',
-              '*': 'undef',
-              '$': 'λ',
-              '=': 'undef'
-              },
-        'T': {'a': 'FR',
-              'b': 'FR',
-              '(': 'FR',
-              ')': 'undef',
-              '+': 'undef',
-              '*': 'undef',
-              '$': 'undef',
-              '=': 'undef'
-              },
-        'R': {'a': 'λ',
-              'b': 'λ',
-              '(': 'λ',
-              ')': 'undef',
-              '+': 'λ',
-              '*': '*FR',
-              '$': 'λ',
-              '=': 'undef'
-              },
-        'F': {'a': 'a',
-              'b': 'b',
-              '(': '(E)',
-              ')': 'undef',
-              '+': 'undef',
-              '*': 'undef',
-              '$': 'undef',
-              '=': 'undef'
-              }
-    }
-
-    parsing_table = {
+    parsing_table_2 = {
         'S': {'a': 'F=E',
               'b': 'F=E',
               '(': 'F=E',
@@ -216,12 +159,19 @@ if __name__ == '__main__':
               }
     }
 
-    terminals = ['a', 'b', '(', ')', '+', '*', '=']
-    input_str = 'a=(a+a)*b$'
-    starting_non_terminal = 'S'
-
-
-    run_parser(parsing_table, input_str, terminals, starting_non_terminal)
+    terminals_2 = ['a', 'b', '(', ')', '+', '*', '=']
+    input_strs_2 = ['a=(a+a)*b$', 'a=a*(b+a)$', 'a=(a+a)b$']
+    starting_non_terminal_2 = 'S'
+    
+    print('Parsing Problem #1')
+    for curr_str in input_strs_1:
+        print('\nResults for ' + curr_str + ': ')
+        run_parser(parsing_table_1, curr_str, terminals_1, starting_non_terminal_1)
+    
+    print('Parsing Problem #2')
+    for curr_str in input_strs_2:
+        print('\nResults for ' + curr_str + ': ')
+        run_parser(parsing_table_2, curr_str, terminals_2, starting_non_terminal_2)
 
 '''
 Sample Output:
